310a2e531b0b2bf312ac87a439875ccad884758d
Mohammed <arrahmaan9901@gmail.com>
2023-06-05 11:22:46 +0530

    after first perm, target_buckets aren't merged

--- Permutations.py
+++ Permutations.py
@@ -235,6 +235,57 @@ import itertools
 #         self.play(*[ball.animate.move_to(ORIGIN) for ball in buckets])
 
 
+# from manim import *
+# from manim.utils.color import *
+
+
+# class Permutations(Scene):
+#     def construct(self):
+#         start_basket = Square(
+#             side_length=1, fill_opacity=0.5, fill_color=YELLOW)
+#         target_buckets = VGroup(
+#             *[Square(side_length=1, fill_opacity=0, stroke_color=WHITE) for _ in range(3)])
+#         target_buckets.arrange(RIGHT, buff=1)
+
+#         colors = [RED, GREEN, BLUE]  # Add more colors if needed
+
+#         balls = VGroup(*[Dot(color=color) for color in colors])
+
+#         self.play(Create(start_basket))
+#         self.play(start_basket.animate.shift(UP * 2))
+
+#         self.wait()
+
+#         self.play(Create(target_buckets))
+
+#         self.play(Write(balls))
+
+#         self.wait()
+
+#         # Generate all permutations of the balls
+#         permutations = self.get_permutations(balls)
+
+#         for permutation in permutations:
+#             self.insert_balls_into_buckets(
+#                 permutation, start_basket, target_buckets)
+#             self.wait(1)
+#             self.clear_balls_from_buckets(target_buckets)
+
+#         self.wait()
+
+#     def get_permutations(self, balls):
+#         return list(itertools.permutations(balls))
+
+#     def insert_balls_into_buckets(self, balls, start_basket, target_buckets):
+#         for ball, bucket in zip(balls, target_buckets):
+#             self.play(ball.animate.move_to(start_basket.get_center()))
+#             self.play(ball.animate.move_to(bucket.get_center() + DOWN * 1.5))
+#             self.play(ball.animate.move_to(bucket))
+
+#     def clear_balls_from_buckets(self, buckets):
+#         self.play(*[ball.animate.move_to(ORIGIN) for ball in buckets])
+
+
 from manim import *
 from manim.utils.color import *
 
@@ -243,9 +294,6 @@ class Permutations(Scene):
     def construct(self):
         start_basket = Square(
             side_length=1, fill_opacity=0.5, fill_color=YELLOW)
-        target_buckets = VGroup(
-            *[Square(side_length=1, fill_opacity=0, stroke_color=WHITE) for _ in range(3)])
-        target_buckets.arrange(RIGHT, buff=1)
 
         colors = [RED, GREEN, BLUE]  # Add more colors if needed
 
@@ -256,8 +304,6 @@ class Permutations(Scene):
 
         self.wait()
 
-        self.play(Create(target_buckets))
-
         self.play(Write(balls))
 
         self.wait()
@@ -265,16 +311,30 @@ class Permutations(Scene):
         # Generate all permutations of the balls
         permutations = self.get_permutations(balls)
 
-        for permutation in permutations:
+        target_buckets = None
+
+        for i, permutation in enumerate(permutations):
+            if i == 0:
+                target_buckets = self.create_target_buckets(permutation)
+            else:
+                self.clear_balls_from_buckets(target_buckets)
+                target_buckets = self.create_target_buckets(permutation)
+
             self.insert_balls_into_buckets(
                 permutation, start_basket, target_buckets)
             self.wait(1)
-            self.clear_balls_from_buckets(target_buckets)
 
         self.wait()
 
     def get_permutations(self, balls):
         return list(itertools.permutations(balls))
+
+    def create_target_buckets(self, balls):
+        target_buckets = VGroup(
+            *[Square(side_length=1, fill_opacity=0.5, fill_color=BLUE_D) for _ in range(len(balls))])
+        target_buckets.arrange(RIGHT, buff=1)
+        self.play(Create(target_buckets))
+        return target_buckets
 
     def insert_balls_into_buckets(self, balls, start_basket, target_buckets):
         for ball, bucket in zip(balls, target_buckets):
