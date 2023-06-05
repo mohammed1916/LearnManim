5b22e823d595eff82fb1e3db1a665c61fb3e57ce
Mohammed <arrahmaan9901@gmail.com>
2023-06-05 11:40:11 +0530

    emerge from start_basket

--- Permutations.py
+++ Permutations.py
@@ -460,6 +460,59 @@ import itertools
 #         self.play(*[ball.animate.move_to(ORIGIN) for ball in buckets])
 
 
+# from manim import *
+# from manim.utils.color import *
+
+
+# class Permutations(Scene):
+#     def construct(self):
+#         start_basket = Square(
+#             side_length=1, fill_opacity=0.5, fill_color=YELLOW)
+#         target_bucket_groups = []
+
+#         colors = [RED, GREEN, BLUE]  # Add more colors if needed
+
+#         balls = VGroup(*[Dot(color=color) for color in colors])
+
+#         self.play(Create(start_basket))
+#         self.play(start_basket.animate.shift(UP * 2))
+
+#         self.wait()
+
+#         self.play(Write(balls))
+
+#         self.wait()
+
+#         # Generate all permutations of the balls
+#         permutations = self.get_permutations(balls)
+
+#         for i, permutation in enumerate(permutations):
+#             target_buckets = self.create_target_buckets(permutation)
+#             target_bucket_groups.append(target_buckets)
+
+#             self.insert_balls_into_buckets(permutation, target_buckets)
+#             self.wait(1)
+
+#         self.wait()
+
+#     def get_permutations(self, balls):
+#         return list(itertools.permutations(balls))
+
+#     def create_target_buckets(self, balls):
+#         target_buckets = VGroup(
+#             *[Square(side_length=1, fill_opacity=0.5, fill_color=BLUE_D) for _ in range(len(balls))])
+#         target_buckets.arrange(RIGHT, buff=1)
+#         self.play(Create(target_buckets))
+#         return target_buckets
+
+#     def insert_balls_into_buckets(self, balls, target_buckets):
+#         for ball, bucket in zip(balls, target_buckets):
+#             self.play(ball.animate.move_to(bucket), run_time=0.5)
+
+#     def clear_balls_from_buckets(self, buckets):
+#         self.play(*[ball.animate.move_to(ORIGIN) for ball in buckets])
+
+
 from manim import *
 from manim.utils.color import *
 
@@ -472,31 +525,33 @@ class Permutations(Scene):
 
         colors = [RED, GREEN, BLUE]  # Add more colors if needed
 
-        balls = VGroup(*[Dot(color=color) for color in colors])
-
         self.play(Create(start_basket))
         self.play(start_basket.animate.shift(UP * 2))
 
         self.wait()
 
-        self.play(Write(balls))
-
-        self.wait()
-
         # Generate all permutations of the balls
-        permutations = self.get_permutations(balls)
+        permutations = self.get_permutations(colors)
 
         for i, permutation in enumerate(permutations):
             target_buckets = self.create_target_buckets(permutation)
             target_bucket_groups.append(target_buckets)
 
-            self.insert_balls_into_buckets(permutation, target_buckets)
+            balls = VGroup(*[Dot(color=color) for color in permutation])
+
+            self.play(Write(balls))
+
+            self.wait()
+
+            self.insert_balls_into_buckets(balls, start_basket, target_buckets)
             self.wait(1)
+
+            self.play(FadeOut(balls))
 
         self.wait()
 
-    def get_permutations(self, balls):
-        return list(itertools.permutations(balls))
+    def get_permutations(self, colors):
+        return list(itertools.permutations(colors))
 
     def create_target_buckets(self, balls):
         target_buckets = VGroup(
@@ -505,8 +560,12 @@ class Permutations(Scene):
         self.play(Create(target_buckets))
         return target_buckets
 
-    def insert_balls_into_buckets(self, balls, target_buckets):
+    def insert_balls_into_buckets(self, balls, start_basket, target_buckets):
         for ball, bucket in zip(balls, target_buckets):
+            self.play(ball.animate.move_to(
+                start_basket.get_center()), run_time=0.5)
+            self.play(ball.animate.move_to(
+                bucket.get_center() + DOWN * 1.5), run_time=0.5)
             self.play(ball.animate.move_to(bucket), run_time=0.5)
 
     def clear_balls_from_buckets(self, buckets):
